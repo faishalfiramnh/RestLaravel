7c583be81e97e9489e56626ac7ef9d2110aff230
isal48 <isalredarmy1@gmail.com>
2018-08-02 09:27:40 +0700

    first

---
+++ database/seeds/PlacesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Http\Controller\PlacesController;
+
+class PlacesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        foreach (range(1, 100) as $i) {
+          $simpan = $faker->domainName();
+          PlacesController::create([
+            'name' =>$faker->streetName.' Places',
+            'namaTempat' =>$faker->namaTempat,
+            'longtitude' =>$faker->longtitude,
+            'lotitude' =>$faker->lotitude
+          ]);
+        }
+    }
+}
